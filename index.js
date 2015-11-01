65004a19f19848c524c2987c785c2de30006c050
Peter Peerdeman <peter@raspi.(none)>
2015-11-01 14:43:05 +0100

    feat(lights): lights on and off in morning and evening

--- index.js
+++ index.js
@@ -2,25 +2,33 @@ var CronJob = require('cron').CronJob;
 var request = require('request');
 var apiUrl = 'http://localhost:3000/api';
 
-var lightsOnWeekdays = new CronJob({
-    cronTime: '00 00 18 * * 1-5',
-    onTick: function() {
-        request(apiUrl + '/lights/on', function(error, response, body) {
-            if (error) return;
-            request('/lights/randomcolors', function(error, response, body) {
-            })
+function lightsOn() {
+    request(apiUrl + '/lights/on', function(error, response, body) {
+        if (error) return;
+        request('/lights/randomcolors', function(error, response, body) {
         })
+    })
+}
+
+function lightsOff() {
+    request(apiUrl + '/lights/off', function(error, response, body) {
+        //
+    })
+}
+
+var lightsOnWeekdays = new CronJob({
+    cronTime: '00 00 07,18 * * 1-5',
+    onTick: function() {
+        lightsOn();
     },
     start: true,
     timeZone: 'Europe/Amsterdam'
 });
 
 var lightsOffWeekdays = new CronJob({
-    cronTime: '00 00 22 * * 1-5',
+    cronTime: '00 00 09,22 * * 1-5',
     onTick: function() {
-        request(apiUrl + '/lights/off', function(error, response, body) {
-            //
-        })
+        lightsOff();
     },
     start: true,
     timeZone: 'Europe/Amsterdam'
