660d00299c7939e69d9f1224d3324e12d0e1e171
Peter Peerdeman <peter@raspi.(none)>
2015-11-21 22:36:03 +0100

    fix(schedule): changed schedule

--- index.js
+++ index.js
@@ -4,20 +4,16 @@ var apiUrl = 'http://localhost:3000/api';
 
 function lightsOn() {
     request(apiUrl + '/lights/on', function(error, response, body) {
-        if (error) return;
-        request('/lights/randomcolors', function(error, response, body) {
-        })
     })
 }
 
 function lightsOff() {
     request(apiUrl + '/lights/off', function(error, response, body) {
-        //
     })
 }
 
-var lightsOnWeekdays = new CronJob({
-    cronTime: '00 00 07,18 * * 1-5',
+var lightsOnWeekdaysMorning = new CronJob({
+    cronTime: '00 00 07 * * 1-5',
     onTick: function() {
         lightsOn();
     },
@@ -25,8 +21,26 @@ var lightsOnWeekdays = new CronJob({
     timeZone: 'Europe/Amsterdam'
 });
 
-var lightsOffWeekdays = new CronJob({
-    cronTime: '00 00 09,22 * * 1-5',
+var lightsOffWeekdaysMorning = new CronJob({
+    cronTime: '00 30 08 * * 1-5',
+    onTick: function() {
+        lightsOff();
+    },
+    start: true,
+    timeZone: 'Europe/Amsterdam'
+});
+
+var lightsOnWeekdaysEvening = new CronJob({
+    cronTime: '00 30 16 * * 1-5',
+    onTick: function() {
+        lightsOn();
+    },
+    start: true,
+    timeZone: 'Europe/Amsterdam'
+});
+
+var lightsOffWeekdaysEvening = new CronJob({
+    cronTime: '00 00 22 * * 1-5',
     onTick: function() {
         lightsOff();
     },
