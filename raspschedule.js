ebb08c9bb4743670410bd750cac485a7199af382
Peter Peerdeman <peter@raspi.(none)>
2016-03-27 00:19:32 +0100

    refactor(file): change filename for better process debugging

--- raspschedule.js
+++ raspschedule.js
@@ -1,6 +1,7 @@
 var CronJob = require('cron').CronJob;
 var request = require('request');
 var apiUrl = 'http://localhost:3000/api';
+var recordFairsTokenUrl = '';
 
 function lightsOn() {
     request(apiUrl + '/lights/on', function(error, response, body) {
@@ -43,6 +44,27 @@ var lightsOffWeekdaysEvening = new CronJob({
     cronTime: '00 00 22 * * 1-5',
     onTick: function() {
         lightsOff();
+    },
+    start: true,
+    timeZone: 'Europe/Amsterdam'
+});
+
+var updateRecordFairs = new CronJob({
+    cronTime: '00 00 00 * * *',
+    onTick: function() {
+        request.post(recordFairsTokenUrl, function(error, response, body) {
+            console.log('updated recordfairs, ', new Date());
+        });
+    },
+    start: true,
+    timeZone: 'Europe/Amsterdam'
+});
+
+var wakeRecordFairs = new CronJob({
+    cronTime: '0 0 8-24 * * *',
+    onTick: function() {
+        request.get('http://recordfairs.nl/favicon-16x16.png', function(error, response, body) {
+        });
     },
     start: true,
     timeZone: 'Europe/Amsterdam'
